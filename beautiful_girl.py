9b2e3d80d78e77dd583f8177bff2b21583be34cc
zhangxutest <18001098773@163.com>
2021-09-22 16:36:45 +0800

    添加23jj

--- beautiful_girl.py
+++ beautiful_girl.py
@@ -7,7 +7,7 @@ import sys
 def download_beautiful_girl():
     os.mkdir("image")
     num = 0
-    page = 7
+    page = 8
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
     }
